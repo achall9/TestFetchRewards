036e4b21c0e472d6e52a57d5124104373f8f2039
Alex Hall <achall@toplev.io>
2021-07-26 10:58:55 -0400

    Init project

---
+++ TestFetchRewards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestFetchRewards
+//
+//  Created by 222 on 26/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
